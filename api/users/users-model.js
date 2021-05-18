0c8d30cde64519bdef6e8db999c5892cf1867a27
Leah King <dancingheart714@yahoo.com>
2021-05-18 15:36:35 -0400

    model wip

--- api/users/users-model.js
+++ api/users/users-model.js
@@ -1,7 +1,6 @@
 const db = require('../../data/db-config.js');
 
-function find() {
-  /**
+/**
     You will need to join two tables.
     Resolves to an ARRAY with all users.
 
@@ -18,10 +17,13 @@ function find() {
       }
     ]
    */
+function find() {
+  return db('users as u')
+    .join('roles as r', 'u.role_id', '=', 'r.role_id')
+    .select('u.user_id', 'u.username', 'r.role_name');
 }
 
-function findBy(filter) {
-  /**
+/**
     You will need to join two tables.
     Resolves to an ARRAY with all users that match the filter condition.
 
@@ -34,10 +36,14 @@ function findBy(filter) {
       }
     ]
    */
+function findBy(filter) {
+  return db('users as u')
+    .join('roles as r', 'u.role_id', '=', 'r.role_id')
+    .select('u.user_id', 'u.username', 'u.password', 'r.role_name')
+    .where(filter);
 }
 
-function findById(user_id) {
-  /**
+/**
     You will need to join two tables.
     Resolves to the user with the given user_id.
 
@@ -47,6 +53,12 @@ function findById(user_id) {
       "role_name": "instructor"
     }
    */
+function findById(user_id) {
+  return db('users as u')
+    .join('roles as r', 'u.role_id', '=', 'r.role_id')
+    .select('u.user_id', 'u.username', r.role_name)
+    .where('u.user_id', user_id)
+    .first();
 }
 
 /**
@@ -67,21 +79,26 @@ function findById(user_id) {
     "role_name": "team lead"
   }
  */
-async function add({ username, password, role_name }) { // done for you
-  let created_user_id
-  await db.transaction(async trx => {
-    let role_id_to_use
-    const [role] = await trx('roles').where('role_name', role_name)
+async function add({ username, password, role_name }) {
+  // done for you
+  let created_user_id;
+  await db.transaction(async (trx) => {
+    let role_id_to_use;
+    const [role] = await trx('roles').where('role_name', role_name);
     if (role) {
-      role_id_to_use = role.role_id
+      role_id_to_use = role.role_id;
     } else {
-      const [role_id] = await trx('roles').insert({ role_name: role_name })
-      role_id_to_use = role_id
+      const [role_id] = await trx('roles').insert({ role_name: role_name });
+      role_id_to_use = role_id;
     }
-    const [user_id] = await trx('users').insert({ username, password, role_id: role_id_to_use })
-    created_user_id = user_id
-  })
-  return findById(created_user_id)
+    const [user_id] = await trx('users').insert({
+      username,
+      password,
+      role_id: role_id_to_use,
+    });
+    created_user_id = user_id;
+  });
+  return findById(created_user_id);
 }
 
 module.exports = {
